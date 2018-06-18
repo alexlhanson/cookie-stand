3dfa1328194ad4cad96725aeeb70acd5e86707eb
Alex Hanson <alexlhanson@gmail.com>
2018-06-18 14:44:29 -0700

    Added cookie calculators

--- js/app.js
+++ js/app.js
@@ -11,48 +11,89 @@
 // Display the values of each array as unordered lists in the browser
 
 // Calculating the sum of these hourly totals; your output for each location should look like this:
-//location objects - literal notation for each location and data associated
+
+//Array for locations
+var locations = [];
+
+//location objects - literal notation for each location and data associated[]
+
 var collegePence = {
   minCust: 23,
   maxCust: 65,
   avgPSales: 6.3,
-  randCust: function (){
-    Math.floor(Math.random() * (maxCust-minCust) + minCust);
-  },
+  cookiesPHour:[],
 };
 
 var chandler14th = {
   minCust: 3,
   maxCust: 24,
   avgPSales: 1.2,
-  randCust: function (){
-    Math.floor(Math.random() * (maxCust-minCust) + minCust);
-  },
+  cookiesPHour:[],
 };
 
 var fresno14th = {
   minCust: 11,
   maxCust: 38,
   avgPSales: 3.7,
-  randCust: function (){
-    Math.floor(Math.random() * (maxCust-minCust) + minCust);
-  },
+  cookiesPHour:[],
 };
 
 var minnesotaBond = {
   minCust: 20,
   maxCust: 38,
   avgPSales: 2.3,
-  randCust: function (){
-    Math.floor(Math.random() * (maxCust-minCust) + minCust);
-  },
+  cookiesPHour:[],
 };
 
 var neffWilliamson = {
   minCust: 2,
   maxCust: 16,
   avgPSales: 4.6,
-  randCust: function (){
-    Math.floor(Math.random() * (maxCust-minCust) + minCust);
-  },
+  cookiesPHour:[],
 };
+
+//calculate cookies per hour
+
+collegePence.cookieCalculator = function (){
+  for (var hours = 0; hours < 16; hours++){
+    var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    this.cookiesPHour.push(Math.floor(randCust * this.avgPSales));
+  }
+};
+
+chandler14th.cookieCalculator = function (){
+  for (var hours = 0; hours < 16; hours++){
+    var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    this.cookiesPHour.push(Math.floor(randCust * this.avgPSales));
+  }
+};
+
+fresno14th.cookieCalculator = function (){
+  for (var hours = 0; hours < 16; hours++){
+    var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    this.cookiesPHour.push(Math.floor(randCust * this.avgPSales));
+  }
+};
+
+minnesotaBond.cookieCalculator = function (){
+  for (var hours = 0; hours < 16; hours++){
+    var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    this.cookiesPHour.push(Math.floor(randCust * this.avgPSales));
+  }
+};
+
+neffWilliamson.cookieCalculator = function (){
+  for (var hours = 0; hours < 16; hours++){
+    var randCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    this.cookiesPHour.push(Math.floor(randCust * this.avgPSales));
+  }
+};
+
+//push objects to locations array
+locations.push(collegePence);
+locations.push(chandler14th);
+locations.push(fresno14th);
+locations.push(minnesotaBond);
+locations.push(neffWilliamson);
+
+  
