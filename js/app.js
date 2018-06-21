e86865f0fb6d171f350c650ff980dd0c8c771c93
Alex Hanson <alexlhanson@gmail.com>
2018-06-20 22:42:12 -0700

    Added stylesheet

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ function Stand(name, minCust, maxCust, avgPSales) {
   this.cookiesPCustomer = [];
   this.cookiesPHour = [];
   this.cookiesTotal = 0;
-  this.hours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
+  this.hours = baseHours;
   
   stands.push(this);
 }
